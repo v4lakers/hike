827c63c4dc057cd93fef113ecc3c6787a46db09c
Vivek Sharath <vsharath@cs.uchicago.edu>
2020-07-02 11:37:35 -0700

    Updating Files

--- hike.py
+++ hike.py
@@ -75,7 +75,7 @@ def map(data_visited, data_yet_to_visit):
         if row["Zip"] not in zip:
             zip.append(row["Zip"])
         else:
-            epsilon = epsilon + random.uniform(0, 1) / 100
+            epsilon = epsilon + random.uniform(0, 1) / 1000
 
         if country == "us":
             info = zips.loc[zips["Zip"] == int(row["Zip"])]
