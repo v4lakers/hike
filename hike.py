a49776e74a39eb73bdc4922b2881e66c89aec1e7
Vivek Sharath <vsharath@cs.uchicago.edu>
2020-09-02 19:25:37 -0700

    Adding Files

--- hike.py
+++ hike.py
@@ -4,6 +4,7 @@ import folium
 import zipcodes
 import pandas as pd
 import gspread
+import matplotlib.pyplot as plt
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 
@@ -75,11 +76,56 @@ def map(data_visited, data_yet_to_visit):
     map.save("index.html")
 
 
+def line_graph(metric, metric_name):
+    values = []
+    dates = []
+
+    for row in metric.iterrows():
+        temp = str(row[1]).split("\n")
+        values.append(float(temp[0].split(" ")[-1]))
+        dates.append(row[0].strftime('%b-%Y'))
+
+    plt.plot(dates, values, 'b-')
+    plt.ylabel(metric_name)
+    plt.xlabel("Month")
+    plt.xticks(rotation=45)
+    plt.title("Hiked " + metric_name + " vs Months")
+
+    for i in range(len(dates)):
+        if i == 0:
+            plt.text(dates[i], values[i] - .4, str(values[i]))
+        elif i == len(dates) - 1:
+            plt.text(dates[i], values[i] + .4, str(values[i]))
+
+        else:
+            if values[i + 1] > values[i]:
+                plt.text(dates[i], values[i] - .4, str(values[i]))
+            else:
+                plt.text(dates[i], values[i] + .4, str(values[i]))
+
+    plt.show()
+
+
+def trailing(data):
+    data['Date'] = pd.to_datetime(data['Date'])
+    data["Length"] = pd.to_numeric(data["Length"])
+    data["Elevation_Gain"] = pd.to_numeric(data["Elevation_Gain"])
+    data["Time"] = pd.to_numeric(data["Time"])
+
+    miles  = data.groupby(data['Date'].dt.to_period("M"))['Length'].sum().to_frame()
+    elev = data.groupby(data['Date'].dt.to_period("M"))['Elevation_Gain'].sum().to_frame()
+    time = data.groupby(data['Date'].dt.to_period("M"))['Time'].sum().to_frame()
+
+    line_graph(miles, "Miles")
+    line_graph(elev, "Elevation Gain")
+    line_graph(time, "Time")
+
 
 
 def main():
     data_visited, data_yet_to_visit = getData()
     map(data_visited, data_yet_to_visit)
+    #trailing(data_visited)
 
 
 main()
