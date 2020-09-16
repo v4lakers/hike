a53b46fdfe356c6b5a2a7cde339c872b684cbb68
Vivek Sharath <vsharath@cs.uchicago.edu>
2020-09-16 16:26:28 -0700

    Updating Dashboard

--- data/style/script.js
+++ data/style/script.js
@@ -66,7 +66,7 @@ $(function () {
   Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.02)';
   Chart.defaults.global.legend.display = false;
 
-var data1 = [54.2, 45.8]
+var data1 = [54.2, 45.8];
   var myChart = new Chart(chart, {
     type: 'doughnut',
     data: {
@@ -97,7 +97,7 @@ var data1 = [54.2, 45.8]
 
 
   // Chart2Here
-var data2 = [14, 36]
+var data2 = [14, 36];
   var Chart2 = document.getElementById('myChart2').getContext('2d');
   var myChart = new Chart(Chart2, {
     type: 'doughnut',
@@ -128,8 +128,8 @@ var data2 = [14, 36]
 
   });
 
-var data3 = [{'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 1.0, 'y': 141.0, 'x': 0.5}], 'label': ['Way of the Mono Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 7.5, 'y': 1500.0, 'x': 3.5}], 'label': ['Upper Yosemite Falls (half)']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 9.0, 'y': 1541.0, 'x': 6.1}], 'label': ['Sanborn Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 17.75, 'y': 1260.0, 'x': 14.1}], 'label': ['Bridge Creek Trail to Maple Falls']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 2.8, 'y': 256.0, 'x': 1.8}], 'label': ['Coal Creek Double Loop']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.15, 'y': 1204.0, 'x': 3.9}], 'label': ['Bishop Peak ']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.0, 'y': 525.0, 'x': 4.3}], 'label': ['Zinfandel Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 6.4, 'y': 771.0, 'x': 3.6}], 'label': ['Saratoga Gap Trail and Ridge Trail Loop']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 7.15, 'y': 1283.0, 'x': 5.0}], 'label': ['San Andreas Trail to Peterson Memorial Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.05, 'y': 755.0, 'x': 3.3}], 'label': ['Lookout, Redwood, Orchard, Creek Trails']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 4.15, 'y': 390.0, 'x': 2.3}], 'label': ['Angel Falls via Willow Creek Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 0.65, 'y': 141.0, 'x': 0.5}], 'label': ['Way of the Mono Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 1.45, 'y': 194.0, 'x': 1.2}], 'label': ['Creekside Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.05, 'y': 640.0, 'x': 4.1}], 'label': ['East Avenue and Ward Creek Trail Loop']}]
-  var labels3 = ['Way of the Mono Trail', 'Upper Yosemite Falls (half)', 'Sanborn Trail', 'Bridge Creek Trail to Maple Falls', 'Coal Creek Double Loop', 'Bishop Peak ', 'Zinfandel Trail', 'Saratoga Gap Trail and Ridge Trail Loop', 'San Andreas Trail to Peterson Memorial Trail', 'Lookout, Redwood, Orchard, Creek Trails', 'Angel Falls via Willow Creek Trail', 'Way of the Mono Trail', 'Creekside Trail', 'East Avenue and Ward Creek Trail Loop']
+var data3 = [{'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 1.0, 'y': 141.0, 'x': 0.5}], 'label': ['Way of the Mono Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 7.5, 'y': 1500.0, 'x': 3.5}], 'label': ['Upper Yosemite Falls (half)']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 9.0, 'y': 1541.0, 'x': 6.1}], 'label': ['Sanborn Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 17.75, 'y': 1260.0, 'x': 14.1}], 'label': ['Bridge Creek Trail to Maple Falls']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 2.8, 'y': 256.0, 'x': 1.8}], 'label': ['Coal Creek Double Loop']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.15, 'y': 1204.0, 'x': 3.9}], 'label': ['Bishop Peak ']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.0, 'y': 525.0, 'x': 4.3}], 'label': ['Zinfandel Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 6.4, 'y': 771.0, 'x': 3.6}], 'label': ['Saratoga Gap Trail and Ridge Trail Loop']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 7.15, 'y': 1283.0, 'x': 5.0}], 'label': ['San Andreas Trail to Peterson Memorial Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.05, 'y': 755.0, 'x': 3.3}], 'label': ['Lookout, Redwood, Orchard, Creek Trails']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 4.15, 'y': 390.0, 'x': 2.3}], 'label': ['Angel Falls via Willow Creek Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 0.65, 'y': 141.0, 'x': 0.5}], 'label': ['Way of the Mono Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 1.45, 'y': 194.0, 'x': 1.2}], 'label': ['Creekside Trail']}, {'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 5.05, 'y': 640.0, 'x': 4.1}], 'label': ['East Avenue and Ward Creek Trail Loop']}];
+  var labels3 = ['Way of the Mono Trail', 'Upper Yosemite Falls (half)', 'Sanborn Trail', 'Bridge Creek Trail to Maple Falls', 'Coal Creek Double Loop', 'Bishop Peak ', 'Zinfandel Trail', 'Saratoga Gap Trail and Ridge Trail Loop', 'San Andreas Trail to Peterson Memorial Trail', 'Lookout, Redwood, Orchard, Creek Trails', 'Angel Falls via Willow Creek Trail', 'Way of the Mono Trail', 'Creekside Trail', 'East Avenue and Ward Creek Trail Loop'];
   var Chart3 = document.getElementById('myChart3').getContext('2d');
   var chart = new Chart(Chart3, {
     axisY: {
@@ -158,8 +158,8 @@ var data3 = [{'backgroundColor': 'rgb(255, 99, 132)', 'data': [{'r': 1.0, 'y': 1
 
   });
 
-var labels5 = ['Aug-2019', 'Dec-2019', 'Jun-2020', 'Jul-2020', 'Aug-2020', 'Sep-2020']
-var data5 = [0.5, 3.5, 20.2, 10.0, 15.9, 4.1]
+var labels5 = ['Aug-2019', 'Dec-2019', 'Jun-2020', 'Jul-2020', 'Aug-2020', 'Sep-2020'];
+var data5 = [0.5, 3.5, 20.2, 10.0, 15.9, 4.1];
 
   var Chart5 = document.getElementById('myChart5').getContext('2d');
   var chart = new Chart(Chart5, {
@@ -198,8 +198,8 @@ var data5 = [0.5, 3.5, 20.2, 10.0, 15.9, 4.1]
     }
     }
   });
-var labels6 = ['2019', '2020']
-var data6 = [5321.0, 50089.0]
+var labels6 = ['2019', '2020'];
+var data6 = [5321.0, 50089.0];
 
   var Chart6 = document.getElementById('myChart6').getContext('2d');
   var chart = new Chart(Chart6, {
